937fdf9f8e20d1f20b30f8a8f389506756456ccf
Luke Reynolds <lukereynoldspi@gmail.com>
2022-11-15 11:55:27 -0800

    finished project

--- threads.py
+++ threads.py
@@ -9,21 +9,36 @@ def runner(name, count):
         time.sleep(0.2)  # seconds
 
 # Launch this many threads
-THREAD_COUNT = 3
+THREAD_COUNT = 10
 
 # We need to keep track of them so that we can join() them later. We'll
 # put all the thread references into this array
 threads = []
-
+result = [0] * THREAD_COUNT
+ranges = [
+    [10, 20],
+    [1, 5],
+    [70, 80],
+    [27, 92],
+    [0, 16],
+    [7, 11],
+    [3, 7],
+    [121, 122],
+    [1, 10],
+    [9, 12]
+]
 # Launch all threads!!
 for i in range(THREAD_COUNT):
-
     # Give them a name
     name = f"Thread{i}"
 
+    start = ranges[i][0]
+    end = ranges[i][1]
+    result[i] = sum(range(start, end)) + end
+
     # Set up the thread object. We're going to run the function called
     # "runner" and pass it two arguments: the thread's name and count:
-    t = threading.Thread(target=runner, args=(name, i+3))
+    t = threading.Thread(target=runner, args=(name, i + THREAD_COUNT))
 
     # The thread won't start executing until we call `start()`:
     t.start()
@@ -37,3 +52,6 @@ for i in range(THREAD_COUNT):
 
 for t in threads:
     t.join()
+
+print(result)
+print(sum(result))
